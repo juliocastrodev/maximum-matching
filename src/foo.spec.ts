c19486d00545d28f168c22a9190aaba00c5a60e0
Julio César Castro López <julio.castro.dev@gmail.com>
2022-02-09 16:28:46 +0100

    feat: add config files

---
+++ src/foo.spec.ts
@@ -0,0 +1,7 @@
+import { foo } from './foo'
+
+describe('foo', () => {
+  it('works', () => {
+    expect(foo(1)).toEqual(1)
+  })
+})
---
+++ src/foo.ts
@@ -0,0 +1,3 @@
+export function foo(num: number): number {
+  return num
+}
